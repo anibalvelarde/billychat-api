f3c00a725f8fd04e10a21c1e02955b0257d0487a
AEV0129 <AEV0129@daugherty.com>
2020-08-01 16:18:01 -0500

    Included NewtonSoft type to handle recursive refs

--- src/BillyChat.API/Startup.cs
+++ src/BillyChat.API/Startup.cs
@@ -60,6 +60,9 @@ namespace BillyChat.API
             services.AddScoped<IAccountRepository, AccountRepository>();
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IAccountService, AccountService>();
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
