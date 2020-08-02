b31707aee83d8966789dc5d5c1fa6970190a0740
AEV0129 <AEV0129@daugherty.com>
2020-08-01 20:44:42 -0500

    Added ability to create account for a user via the Accounts Resource.

--- src/BillyChat.API/Controllers/AccountsController.cs
+++ src/BillyChat.API/Controllers/AccountsController.cs
@@ -1,4 +1,5 @@
 using BillyChat.API.Domain.Models;
+using BillyChat.API.Domain.Models.Enums;
 using BillyChat.API.Domain.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -15,14 +16,37 @@ namespace BillyChat.API.Controllers
     public class AccountsController : Controller
     {
         private readonly IAccountService _accountsServcie;
+        private readonly IUserService _userService;
 
-        public AccountsController(IAccountService acctSvc) => _accountsServcie = acctSvc;
+        public AccountsController(IAccountService acctSvc, IUserService userSvcv) 
+        { 
+            _accountsServcie = acctSvc;
+            _userService = userSvcv;
+        }
 
         [HttpGet]
         public async Task<IEnumerable<Account>> GetAllAccountsAsync()
         {
             IEnumerable<Account> accounts = await _accountsServcie.ListAsync();
             return accounts;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAccountAsync(int userId, AccountType ofType)
+        {
+            try
+            {
+                var aUser = await _userService.GetByIdAsync(userId);
+                await _accountsServcie.CreateAsync(
+                    aUser,
+                    ofType
+                );
+                return Ok();
+            }
+            catch (ApplicationException)
+            {
+                return NotFound();
+            }
         }
     }
 }
